0e8fc5ff0e05b9c47ec2fffd411324839e84c559
VitorMalvestiti <malvestiti.vitor@gmail.com>
2025-09-18 21:17:36 -0300

    add dependencias do service no controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,11 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { SignInDTO, SignUpDTO } from './dtos/auth';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+
+    constructor(private authService: AuthService){}
 
     @Post('signup')
     async signup(@Body() body: SignUpDTO) { 
