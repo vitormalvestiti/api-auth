d25c6cbfb9671ba31f939ec5ca7b4ad2301ddc50
VitorMalvestiti <malvestiti.vitor@gmail.com>
2025-09-18 23:36:13 -0300

    create prisma service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,13 @@
 import { Body, Injectable } from '@nestjs/common';
 import { async } from 'rxjs';
 import { SignInDTO, SignUpDTO } from './dtos/auth';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
     
+    constructor(private prismaService: PrismaService){}
+
     async signup(@Body() body: SignUpDTO) {
         console.log(body);
         return body;
